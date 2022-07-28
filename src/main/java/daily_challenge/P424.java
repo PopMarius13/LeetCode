19ad1ca1c4b077c9ae23d4705b7e78762ef4a215
PopMarius13 <popmariusionut13@gmail.com>
2022-07-28 20:00:13 +0300

    P424

---
+++ src/main/java/daily_challenge/P424.java
@@ -0,0 +1,52 @@
+package daily_challenge;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P424 {
+
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            int n = s.length();
+            if (n != t.length()) {
+                return false;
+            }
+            Map<Character, Integer> letters = new HashMap<>();
+            for (int i = 0; i < n; i++) {
+                char c = s.charAt(i);
+                letters.put(c, letters.getOrDefault(s.charAt(i), 0) + 1);
+                c = t.charAt(i);
+                letters.put(c, letters.getOrDefault(s.charAt(i), 0) - 1);
+            }
+            System.out.println(letters);
+            for (int v : letters.values()) {
+                if (v != 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    class Solution1 {
+        public boolean isAnagram(String s, String t) {
+            var freq = new int[26];
+            if(s.length() != t.length()) {
+                return false;
+            }
+
+            for (int i = 0; i < s.length(); i++) {
+                freq[s.charAt(i) - 'a']++;
+                freq[t.charAt(i) - 'a']--;
+            }
+
+            for (int i = 0; i <= 26; ++i) {
+                if (freq[i] != 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
